d1011601d74f20b8f378e5f334d6c6b650349284
MusaevBeK <beksultan1923@gmail.com>
2022-09-26 00:48:00 +0600

    Initial Commit

---
+++ BottomSheetLanguage/BottomSheetLanguageApp.swift
@@ -0,0 +1,17 @@
+//
+//  BottomSheetLanguageApp.swift
+//  BottomSheetLanguage
+//
+//  Created by user on 26/9/22.
+//
+
+import SwiftUI
+
+@main
+struct BottomSheetLanguageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
